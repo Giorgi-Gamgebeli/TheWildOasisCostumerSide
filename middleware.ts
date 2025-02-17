8fe31ab2c68c9e45d919bb67b316966b5e209104
CodingReaper1 <giorgi.gamertube@gmail.com>
2025-02-18 01:36:00 +0400

    small update in middleware

--- middleware.ts
+++ middleware.ts
@@ -11,39 +11,34 @@ import { NextRequest } from "next/server";
 
 const { auth: middleware } = NextAuth(authConfig);
 
-export default middleware(
-  (req: NextRequest & { auth: Session | null }): Response | void => {
-    const userAgent = req.headers.get("user-agent") || "";
-    const isCrawler = /Googlebot|Bingbot|YandexBot/.test(userAgent);
+export default middleware((req: NextRequest & { auth: Session | null }) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-    const { nextUrl } = req;
-    const isLoggedIn = !!req.auth;
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
 
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isFullPublicRoute = fullPublicRoutes.some((fullPublicRoute) => {
+    if (nextUrl.pathname.startsWith(fullPublicRoute)) return true;
+  });
 
-    const isFullPublicRoute = fullPublicRoutes.some((fullPublicRoute) => {
-      if (nextUrl.pathname.startsWith(fullPublicRoute)) return true;
-    });
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  if (isApiAuthRoute || isFullPublicRoute) return;
 
-    if (isApiAuthRoute || isFullPublicRoute || isCrawler) return;
-
-    if (isAuthRoute) {
-      if (isLoggedIn) {
-        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-      }
-      return;
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-
-    if (!isLoggedIn && !isPublicRoute) {
-      return Response.redirect(new URL("/login", nextUrl));
-    }
-
     return;
   }
-);
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/login", nextUrl));
+  }
+
+  return;
+});
 
 // export const config = {
 //   matcher: [
