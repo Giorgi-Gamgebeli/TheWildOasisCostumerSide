aaa98fe6d313f7b2ba82048ace9379ec1bee9602
CodingReaper1 <giorgi.gamertube@gmail.com>
2025-02-18 01:31:27 +0400

    fixing middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -45,11 +45,17 @@ export default middleware(
   }
 );
 
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|sitemap/sitemap.xml).*)",
   ],
 };
